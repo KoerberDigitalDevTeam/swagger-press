9f3a70f8a5a6d4951589a01803ef776c625c26b7
Pier Fumagalli <pier@usrz.com>
2018-08-29 15:26:04 +0200

    Reworking headers into a class (with proxy)

---
+++ classes/headers.js
@@ -0,0 +1,326 @@
+'use strict'
+
+/* ========================================================================== *
+ * STATIC DATA                                                                *
+ * ========================================================================== */
+
+/*
+ * Our collection of known headers from
+ * https://en.wikipedia.org/wiki/List_of_HTTP_header_fields
+ */
+const headers = Object.freeze([
+  'A-IM',
+  'Accept',
+  'Accept-Charset',
+  'Accept-Datetime',
+  'Accept-Encoding',
+  'Accept-Language',
+  'Accept-Patch',
+  'Accept-Ranges',
+  'Access-Control-Allow-Credentials',
+  'Access-Control-Allow-Headers',
+  'Access-Control-Allow-Methods',
+  'Access-Control-Allow-Origin',
+  'Access-Control-Expose-Headers',
+  'Access-Control-Max-Age',
+  'Access-Control-Request-Headers',
+  'Access-Control-Request-Method',
+  'Age',
+  'Allow',
+  'Alt-Svc',
+  'Authorization',
+  'Cache-Control',
+  'Connection',
+  'Content-Disposition',
+  'Content-Encoding',
+  'Content-Language',
+  'Content-Length',
+  'Content-Location',
+  'Content-MD5',
+  'Content-Range',
+  'Content-Security-Policy',
+  'Content-Type',
+  'Cookie',
+  'DNT',
+  'Date',
+  'Delta-Base',
+  'ETag',
+  'Expect',
+  'Expires',
+  'Forwarded',
+  'From',
+  'Front-End-Https',
+  'Host',
+  'IM',
+  'If-Match',
+  'If-Modified-Since',
+  'If-None-Match',
+  'If-Range',
+  'If-Unmodified-Since',
+  'Last-Modified',
+  'Link',
+  'Location',
+  'Max-Forwards',
+  'Origin',
+  'P3P',
+  'Pragma',
+  'Proxy-Authenticate',
+  'Proxy-Authorization',
+  'Proxy-Connection',
+  'Public-Key-Pins',
+  'Range',
+  'Referer',
+  'Refresh',
+  'Retry-After',
+  'Save-Data',
+  'Server',
+  'Set-Cookie',
+  'Status',
+  'Strict-Transport-Security',
+  'TE',
+  'Timing-Allow-Origin',
+  'Tk',
+  'Trailer',
+  'Transfer-Encoding',
+  'Upgrade',
+  'Upgrade-Insecure-Requests',
+  'User-Agent',
+  'Vary',
+  'Via',
+  'WWW-Authenticate',
+  'Warning',
+  'X-ATT-DeviceId',
+  'X-Content-Duration',
+  'X-Content-Security-Policy',
+  'X-Content-Type-Options',
+  'X-Correlation-ID',
+  'X-Csrf-Token',
+  'X-Forwarded-For',
+  'X-Forwarded-Host',
+  'X-Forwarded-Proto',
+  'X-Frame-Options',
+  'X-Http-Method-Override',
+  'X-Powered-By',
+  'X-Request-ID',
+  'X-Requested-With',
+  'X-UA-Compatible',
+  'X-UIDH',
+  'X-Wap-Profile',
+  'X-WebKit-CSP',
+  'X-XSS-Protection',
+])
+
+/*
+ * Prepare a map keyed by the lower-case representation of the header, and
+ * with the value as the normal (Camel-Case) name of the header
+ */
+const mappings = Object.freeze(headers.reduce((map, name) => {
+  map[name.toLowerCase()] = name
+  return map
+}, {}))
+
+/*
+ *
+ */
+function processCommas(values, callback) {
+  let result = []
+  values.forEach((value) => {
+    value.replace(/\s/g, '')
+         .replace(/,+/g, ',')
+         .split(',')
+         .forEach((component) => {
+      let processed = callback(component)
+      if (processed) result.push(processed)
+    })
+  })
+  return [ result.join(', ') ]
+}
+
+/*
+ * Normalise a header name into its canonical representation, with a "key"
+ * being the lower-cased version of the name, and the "name" being it's
+ * (possibly normalised) Camel-Case version
+ */
+function normalise(header, value) {
+  /* Triple check header name type and length */
+  if (typeof header !== 'string') {
+    throw new TypeError('Header name must be of type string')
+  }
+
+  /* Normalise header key and name */
+  let key = header.toLowerCase().trim()
+  let name = mappings[key] || header.trim()
+
+  if ((!key) || (!name)) {
+    throw new TypeError('Header name must be a non-empty string')
+  }
+
+  /* Shortctut when no value is given */
+  if (value == null) return { key, name, values: [] }
+
+  /* Normalise a header value to an array */
+  let values = (Array.isArray(value) ? value : [ value ])
+    .reduce((array, value) => {
+      if (value != null) value = value.toString()
+      if (value) array.push(value)
+      return array
+    }, [])
+
+  /* Normalise Access-Control-...-(Headers|Methods) values */
+  if (/^access-control-(allow|expose|request)-headers$/.test(key)) {
+    values = processCommas(values, (component) => normalise(component).name)
+  } else if (key == 'access-control-allow-methods') {
+    values = processCommas(values, (component) => component.trim().toUpperCase())
+  }
+
+  /* Return our nomalised version */
+  return { key, name, values }
+}
+
+/* ========================================================================== *
+ * HEADERS STRUCTURE                                                          *
+ * ========================================================================== */
+
+class Headers {
+  constructor(object) {
+    Object.defineProperty(this, '__data', { writable: false, value: {} })
+
+    /* From array [ { 'content-type': 'foo' }, { 'x-header': [ 'bar', 'baz' ] } ] */
+    if (Array.isArray(object)) {
+      object.forEach((entry) => {
+        for (let key in entry) {
+          if (key in entry) this.add(key, entry[key])
+        }
+      })
+
+    /* From object { 'content-type': 'foo', 'x-header': [ 'bar', 'baz' ] } */
+    } else if (object && (typeof object === 'object')) {
+      for (let key in object) {
+        if (key in object) this.add(key, object[key])
+      }
+    }
+  }
+
+  keys() {
+    return Object.keys(this.__data).map((key) => this.__data[key].name)
+  }
+
+  values() {
+    let result = {}
+    for (let key of Object.keys(this.__data)) {
+      let entry = this.__data[key]
+      result[entry.name] = entry.values
+    }
+    return result
+  }
+
+  add(header, value) {
+    /* Normalise header name and value */
+    let { key, name, values } = normalise(header, value)
+
+    /* No header values to add, bail out */
+    if (values.length == 0) return this
+
+    /* Add (to existing) data */
+    let entry = this.__data[key]
+    if (! entry) {
+      this.__data[key] = { name, values }
+    } else if (/^access-control-(allow|expose|request)-headers$/.test(key) ||
+               (key == 'access-control-allow-methods')) {
+      entry.values[0] += ', ' + values
+    } else {
+     entry.values = entry.values.concat(values)
+    }
+
+    /* Chaining */
+    return this
+  }
+
+  set(header, value) {
+    /* Normalise header name and value */
+    let { key, name, values } = normalise(header, value)
+
+    /* No header values means delete */
+    if (values.length == 0) {
+      delete this.__data[key]
+      return this
+    }
+
+    /* If we have a previous definition, copy the name */
+    name = (this.__data[key] && this.__data[key].name) || name
+    this.__data[key] = { name, values }
+
+    /* Chaining */
+    return this
+  }
+
+  del(header) {
+    let { key } = normalise(header)
+    delete this.__data[key]
+    return this
+  }
+
+  get(header) {
+    let { key } = normalise(header)
+
+    let entry = this.__data[key]
+    if (! entry) return undefined
+    return entry.values
+  }
+}
+
+module.exports = function HeadersProxy(object) {
+  return new Proxy(new Headers(object), {
+    get: (target, property) => {
+      /* Declared properties and symbols */
+      if (property in target) return target[property]
+      if (typeof property !== 'string') return target[property]
+
+      /* Return the first value for the header */
+      return (target.get(property) || [])[0]
+    },
+
+    set: (target, property, value) => {
+      /* Declared properties and symbols */
+      if (property in target) return target[property] = value
+      if (typeof property !== 'string') return target[property] = value
+
+      /* Set the header value */
+      target.set(property, value)
+      return true
+    },
+
+    has: (target, property) => {
+      /* Declared properties and symbols */
+      if (property in target) return true
+      if (typeof property !== 'string') property in target
+
+      /* Check if we have the property */
+      return target.get(property) != null
+    },
+
+    ownKeys: (target) => {
+      return Reflect.ownKeys(target).concat(target.keys())
+    },
+
+    getOwnPropertyDescriptor(target, property) {
+      let descriptor = Object.getOwnPropertyDescriptor(target, property)
+      if (descriptor) return descriptor
+
+      if (target.get(property) == null) return undefined
+      return { configurable: true, enumerable: true }
+    },
+
+    deleteProperty: (target, property) => {
+      /* Declared properties and symbols */
+      if (property in target) return delete target[property]
+      if (typeof property !== 'string') return delete target[property]
+
+      /* Delete the header value */
+      target.del(property)
+      return true
+    },
+  })
+}
+
+module.exports.Headers = Headers
--- test/headers.test.js
+++ test/headers.test.js
@@ -1,64 +1,226 @@
 'use strict'
 
 const { expect } = require('chai')
-const normalise = require('../lib/headers')
+const Headers = require('../classes/headers')
 
-describe('Headers normalisation', () => {
-  it('should normalise a string', () => {
-    expect(normalise('  content-type  ')).to.equal('Content-Type')
-    expect(normalise('  CONTENT-TYPE  ' )).to.equal('Content-Type')
-
-    expect(normalise('  x-foo-bar  ')).to.equal('x-foo-bar')
-    expect(normalise('  X-FOO-BAR  ')).to.equal('X-FOO-BAR')
-  })
-
-  it('should normalise an array', () => {
-    expect(normalise([
-      '  content-type  ',
-      '  CONTENT-TYPE  ',
-      '  x-foo-bar  ',
-      '  X-FOO-BAR  ',
-    ])).to.eql([
-      'Content-Type',
-      'Content-Type',
-      'x-foo-bar',
-      'X-FOO-BAR',
-    ])
-  })
-
-  it('should normalise an object', () => {
-    expect(normalise({
-      '  content-type  ': 1, // note that the resulting object will
-      '  CONTENT-TYPE  ': 2, // collapse thse two headers in one
-      '  x-foo-bar  ': 3,
-      '  X-FOO-BAR  ': 4,
-    })).to.eql({
-      'Content-Type': 2, // collapsed header
-      'x-foo-bar': 3,
-      'X-FOO-BAR': 4,
+describe('Headers class', () => {
+  describe('Construction', () => {
+    it('should construct a Headers instance', () => {
+      expect(Headers).to.be.a('function')
+      expect(new Headers()).to.eql({})
     })
 
-    expect(normalise({ '  content-type  ': 1 })).to.eql({ 'Content-Type': 1 })
-    expect(normalise({ '  CONTENT-TYPE  ': 1 })).to.eql({ 'Content-Type': 1 })
-  })
+    it('should construct a Headers instance with an object', () => {
+      let h = new Headers({
+        'Content-Type': 'test1',
+        ' content-type ': 'test2',
+        'X-Foo': [ 'hello', 'world' ],
+        ' x-foo ': [ 'foo', 'bar', 'baz' ],
+      })
 
-  it('should normalise the value of all Access-Control-...-Headers headers', () => {
-    expect(normalise({
-      '  access-control-allow-headers  ': ' content-type, CONTENT-LENGTH, SeRvEr ,X-FOO-BAR ',
-      '  ACCESS-CONTROL-EXPOSE-HEADERS  ': ' content-type, CONTENT-LENGTH, SeRvEr ,X-FOO-BAR ',
-      '  Access-Control-Request-Headers  ': ' content-type, CONTENT-LENGTH, SeRvEr ,X-FOO-BAR ',
-    })).to.eql({
-      'Access-Control-Allow-Headers': 'Content-Type, Content-Length, Server, X-FOO-BAR',
-      'Access-Control-Expose-Headers': 'Content-Type, Content-Length, Server, X-FOO-BAR',
-      'Access-Control-Request-Headers': 'Content-Type, Content-Length, Server, X-FOO-BAR',
+      expect(h).to.eql({
+        'Content-Type': 'test1',
+        'X-Foo': 'hello',
+      })
+
+      expect(h.keys()).to.eql([ 'Content-Type', 'X-Foo' ])
+
+      expect(h.values()).to.eql({
+        'Content-Type': [ 'test1', 'test2' ],
+        'X-Foo': [ 'hello', 'world', 'foo', 'bar', 'baz' ],
+      })
+    })
+
+    it('should construct a Headers instance with an array', () => {
+      let h = new Headers([
+        { 'Content-Type': 'test1' },
+        { ' content-type ': 'test2' },
+        { 'X-Foo': [ 'hello', 'world' ] },
+        { ' x-foo ': [ 'foo', 'bar', 'baz' ] },
+      ])
+
+      expect(h).to.eql({
+        'Content-Type': 'test1',
+        'X-Foo': 'hello',
+      })
+
+      expect(h.keys()).to.eql([ 'Content-Type', 'X-Foo' ])
+
+      expect(h.values()).to.eql({
+        'Content-Type': [ 'test1', 'test2' ],
+        'X-Foo': [ 'hello', 'world', 'foo', 'bar', 'baz' ],
+      })
     })
   })
 
-  it('should normalise the value of the Access-Control-Allow-Methods header', () => {
-    expect(normalise({
-      '  access-control-allow-methods  ': ' GET, post, OpTiOnS ,paTCH ',
-    })).to.eql({
-      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PATCH',
+  /* ======================================================================== */
+
+  describe('Setting', () => {
+    it('should override headers with the "set()" method', () => {
+      let h = new Headers({ 'X-Foo': 'bar' })
+
+      expect(h).to.eql({ 'X-Foo': 'bar' })
+      expect(h.values()).to.eql({ 'X-Foo': [ 'bar' ] })
+
+      h.set('x-foo', [ 'baz', 'xyz' ])
+
+      expect(h).to.eql({ 'X-Foo': 'baz' })
+      expect(h.values()).to.eql({ 'X-Foo': [ 'baz', 'xyz' ] })
+    })
+
+    it('should override headers with a setter', () => {
+      let h = new Headers({ 'X-Foo': 'bar' })
+
+      expect(h).to.eql({ 'X-Foo': 'bar' })
+      expect(h.values()).to.eql({ 'X-Foo': [ 'bar' ] })
+
+      h['x-foo'] = [ 'baz', 'xyz' ]
+
+      expect(h).to.eql({ 'X-Foo': 'baz' })
+      expect(h.values()).to.eql({ 'X-Foo': [ 'baz', 'xyz' ] })
+    })
+  })
+
+  /* ======================================================================== */
+
+  describe('Deletion', () => {
+    it('should delete headers with the "set()" method', () => {
+      let h = new Headers({ 'X-Foo': 'bar' })
+
+      expect(h).to.eql({ 'X-Foo': 'bar' })
+      expect(h.values()).to.eql({ 'X-Foo': [ 'bar' ] })
+
+      h.set('x-foo')
+
+      expect(h).to.eql({})
+      expect(h.values()).to.eql({})
+    })
+
+    it('should delete headers with a setter', () => {
+      let h = new Headers({ 'X-Foo': 'bar' })
+
+      expect(h).to.eql({ 'X-Foo': 'bar' })
+      expect(h.values()).to.eql({ 'X-Foo': [ 'bar' ] })
+
+      h['x-foo'] = null
+
+      expect(h).to.eql({})
+      expect(h.values()).to.eql({})
+    })
+
+    it('should delete headers with the "del()" method', () => {
+      let h = new Headers({ 'X-Foo': 'bar' })
+
+      expect(h).to.eql({ 'X-Foo': 'bar' })
+      expect(h.values()).to.eql({ 'X-Foo': [ 'bar' ] })
+
+      h.del('x-foo')
+
+      expect(h).to.eql({})
+      expect(h.values()).to.eql({})
+    })
+
+    it('should delete headers with the delete keyword', () => {
+      let h = new Headers({ 'X-Foo': 'bar' })
+
+      expect(h).to.eql({ 'X-Foo': 'bar' })
+      expect(h.values()).to.eql({ 'X-Foo': [ 'bar' ] })
+
+      delete h['x-foo']
+
+      expect(h).to.eql({})
+      expect(h.values()).to.eql({})
+    })
+  })
+
+  /* ======================================================================== */
+
+  describe('Appending', () => {
+    it('should append header values "add()" method', () => {
+      let h = new Headers({ 'X-Foo': 'bar' })
+
+      expect(h).to.eql({ 'X-Foo': 'bar' })
+      expect(h.values()).to.eql({ 'X-Foo': [ 'bar' ] })
+
+      h.add('x-foo', 'baz')
+      h.add('X-FOO', [ 'xyz', 'abc' ])
+      h.add('X-None-1', 'hello 1' )
+      h.add('X-None-2', [ 'hello 2', 'world' ])
+
+      expect(h).to.eql({
+        'X-Foo': 'bar',
+        'X-None-1': 'hello 1',
+        'X-None-2': 'hello 2',
+      })
+
+      expect(h.values()).to.eql({
+        'X-Foo': [ 'bar', 'baz', 'xyz', 'abc' ],
+        'X-None-1': [ 'hello 1' ],
+        'X-None-2': [ 'hello 2', 'world' ],
+      })
+    })
+  })
+
+  /* ======================================================================== */
+
+  describe('Normalization', () => {
+    it('should normalise the value of all Access-Control-...-Headers headers', () => {
+      expect(new Headers({
+        '  access-control-allow-headers  ': ' content-type, CONTENT-LENGTH, SeRvEr ,X-FOO-BAR ',
+        '  ACCESS-CONTROL-EXPOSE-HEADERS  ': [ 'content-type', 'CONTENT-LENGTH', 'SeRvEr', 'X-FOO-BAR' ],
+        '  Access-Control-Request-Headers  ': [ 'content-type,,,CONTENT-LENGTH', 'SeRvEr ,X-FOO-BAR' ],
+      }).values()).to.eql({
+        'Access-Control-Allow-Headers': [ 'Content-Type, Content-Length, Server, X-FOO-BAR' ],
+        'Access-Control-Expose-Headers': [ 'Content-Type, Content-Length, Server, X-FOO-BAR' ],
+        'Access-Control-Request-Headers': [ 'Content-Type, Content-Length, Server, X-FOO-BAR' ],
+      })
+    })
+
+    it('should normalise the value of all Access-Control-...-Headers headers when "add()" is used', () => {
+      let h = new Headers({
+        '  access-control-allow-headers  ': ' content-type, CONTENT-LENGTH, SeRvEr ,X-FOO-BAR ',
+        '  ACCESS-CONTROL-EXPOSE-HEADERS  ': [ 'content-type', 'CONTENT-LENGTH', 'SeRvEr', 'X-FOO-BAR' ],
+        '  Access-Control-Request-Headers  ': [ 'content-type,,,CONTENT-LENGTH', 'SeRvEr ,X-FOO-BAR' ],
+      }).add('ACCESS-CONTROL-EXPOSE-HEADERS', 'Content-Transfer-Encoding,,,X-Minion')
+        .add('Access-Control-Request-Headers', 'Content-Transfer-Encoding, X-Minion  ')
+        .add('access-control-allow-headers', [ 'Content-Transfer-Encoding', '  X-Minion ' ])
+
+      expect(h.values()).to.eql({
+        'Access-Control-Allow-Headers': [ 'Content-Type, Content-Length, Server, X-FOO-BAR, Content-Transfer-Encoding, X-Minion' ],
+        'Access-Control-Expose-Headers': [ 'Content-Type, Content-Length, Server, X-FOO-BAR, Content-Transfer-Encoding, X-Minion' ],
+        'Access-Control-Request-Headers': [ 'Content-Type, Content-Length, Server, X-FOO-BAR, Content-Transfer-Encoding, X-Minion' ],
+      })
+    })
+
+    it('should normalise the value of the Access-Control-Allow-Methods header', () => {
+      expect(new Headers({
+        '  access-control-allow-methods  ': ' GET, post, OpTiOnS ,paTCH ',
+      }).values()).to.eql({
+        'Access-Control-Allow-Methods': [ 'GET, POST, OPTIONS, PATCH' ],
+      })
+
+      expect(new Headers({
+        '  access-control-allow-methods  ': [ 'GET', 'post', 'OpTiOnS', 'paTCH' ],
+      }).values()).to.eql({
+        'Access-Control-Allow-Methods': [ 'GET, POST, OPTIONS, PATCH' ],
+      })
+
+      expect(new Headers({
+        '  access-control-allow-methods  ': [ 'GET,,,post', '  OpTiOnS , , , paTCH ' ],
+      }).values()).to.eql({
+        'Access-Control-Allow-Methods': [ 'GET, POST, OPTIONS, PATCH' ],
+      })
+    })
+
+    it('should normalise the value of the Access-Control-Allow-Methods header when "add()" is used', () => {
+      let h = new Headers({ '  access-control-allow-methods  ': ' GET, post, ' })
+        .add('Access-Control-Allow-Methods', ' OpTiOnS ')
+        .add('Access-Control-Allow-Methods', [ 'put', ' patch' ])
+        .add('Access-Control-Allow-Methods', [ '  lock,,,, UNLOCK ' ])
+
+      expect(h.values()).to.eql({
+        'Access-Control-Allow-Methods': [ 'GET, POST, OPTIONS, PUT, PATCH, LOCK, UNLOCK' ],
+      })
     })
   })
 })
